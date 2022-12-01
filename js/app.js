60499013e13515baf37025dd3f9482191efbe400
billytrinh <quanghoa.trinh@gmail.com>
2022-12-01 16:55:13 +0700

    code bootstrap

---
+++ js/app.js
@@ -0,0 +1,23 @@
+console.log("Hello world!");
+let x = 10;
+x = "hello";
+let y= 20;
+console.log(y+100);//120
+console.log(y+x);// 20hello
+console.log(x+y);// hello20
+let z = `value of y = ${y} Finish.`;
+console.log(z);
+let t = true;
+let n = null;
+
+if(y > 10){
+
+}else{
+
+}
+
+switch(y){
+    case 10: console.log("A"); break;
+    case 20: console.log("B"); break;
+    case 30: console.log("C"); break;
+}
